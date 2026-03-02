9a30112a3e00e24139d23e8870a823172de53051
Mead <meadsabit@gmail.com>
2026-03-02 17:23:31 +0200

    Add generic ServiceListPage template with dynamic rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import EmergencyDispatch from "./components/emergencyDispatch/emergencyDispatch"
 import Footer from "./components/Footer/Footer";
 import Maps from "./Maps";
 import Home from "./pages/Home";
-
+import ServiceDetailsCard from "./components/ServiceDetailsCard/ServiceDetailsCard";
 function App() {
   return (
     <>
       {/* <Maps /> */}
       {/* <Home /> */}
-      <Footer />
+      <ServiceDetailsCard />
+      
     </>
   );
 }
